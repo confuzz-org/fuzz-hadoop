a29dbf1ac34004b2f30f4221476e0bbf535411f9
shuaiwang516 <shuaiwang516@gmail.com>
2022-10-18 21:41:33 -0500

    update scripts

--- hadoop-common-project/hadoop-common/parse_failure.sh
+++ hadoop-common-project/hadoop-common/parse_failure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-OUTPUT_FILE=$1
-find fuzz_output/fuzz-results/ -regex ".*/id.*" | grep failures | sort > ${OUTPUT_FILE}
+FUZZ_OUTPUT_DIR=$1
+RESULT_OUTPUT_FILE=$2
+find ${FUZZ_OUTPUT_DIR}/fuzz-results/ -regex ".*/id.*" | grep failures | sort > ${RESULT_OUTPUT_FILE}
