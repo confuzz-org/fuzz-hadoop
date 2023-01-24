271df3e839e98bb2ff10473da81a9205290d192d
shuaiwang516 <shuaiwang516@gmail.com>
2023-01-23 21:22:59 -0600

    hdfs debug test

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/TestDebug.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/TestDebug.java
@@ -1,32 +1,25 @@
 package org.apache.hadoop;
+import org.apache.hadoop.hdfs.HdfsConfiguration;
 import org.junit.Test;
 import org.apache.hadoop.conf.Configuration;
 
 public class TestDebug {
     @Test
     public void test(/*@From(ConfigurationGenerator.class) Configuration generatedConfig*/) throws Exception {
-        Configuration conf = new Configuration();
+        HdfsConfiguration conf = new HdfsConfiguration();
         int count = 0;
-        String name = conf.get("myname");
-        String age = conf.get("myage");
-        System.out.println("name: " + name + ", age: " + age);
-        String str = conf.get("fs.s3a.select.output.csv.quote.fields");
-        String str2 = conf.get("hadoop.http.staticuser.user");
-        System.out.println(str2);
-        conf.set("fake2", "200");
-        conf.set("fake3", "300");
-        //System.out.println(str);
-        if (str.equals("always")) {
-            System.out.println("always");
+        String str = conf.get("dfs.http.policy");
+        System.out.println(str);
+        if (str.equals("HTTP_ONLY")) {
+            System.out.println("HTTP_ONLY");
             count ++;
-        } else if (str.equals("asneeded")) {
-            System.out.println("asneeded");
+        } else if (str.equals("HTTP_AND_HTTPS")) {
+            System.out.println("HTTP_AND_HTTPS");
             conf.set("fake-config1","15");
             count --;
-            throw new Exception("Fake Bug asneeded");
+            throw new Exception("Fake Bug HTTP_AND_HTTPS");
         } else {
             System.out.println(str);
         }
-        //System.out.println("Conf Length : " + conf.size());
     }
 }
