5b03bbc8089ad2116a6a1e78be5c5d45afd3f705
Tony Wang <tony.wanghao@stu.pku.edu.cn>
2023-03-10 18:49:43 +0000

    fix permission issue

--- docker-fuzz.sh
+++ docker-fuzz.sh
@@ -6,12 +6,11 @@ constraintFile=constraint
 regexFile=regex
 duration=$4
 
-docker run --name ${containerName} -u ctestfuzz -d -i -t "hadoop-build" bash
-docker exec -u ctestfuzz ${containerName} \
-    "cd fuzz-hadoop/${testModule} && mvn confuzz:fuzz -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile} -Dmeringue.duration=${duration}" # Do not run in detached mode!
-docker exec -u ctestfuzz ${containerName} \
-    "cd ${testModule} && mvn confuzz:analyze -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile} -Dmeringue.duration=${duration}" # Do not run in detached mode!
-mkdir -p result/$testClass/$testMethod/output
-docker cp -r "${containerName}:/home/ctestfuzz/fuzz-hadoop/$testModule/meringue" "result/$testClass/$testMethod/output"
+docker run --name ${containerName} -u ctestfuzz -w "/home/ctestfuzz/fuzz-hadoop/${testModule}" -d -i -t "hadoop-build" bash
+docker exec -u ctestfuzz ${containerName} mvn confuzz:fuzz -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile} -Dmeringue.duration=${duration}
+docker exec -u ctestfuzz ${containerName} mvn confuzz:analyze -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile} -Dmeringue.duration=${duration}
+docker exec -u ctestfuzz ${containerName} mvn confuzz:debug -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile}
+mkdir -p result/$testClass
+docker cp -r "${containerName}:/home/ctestfuzz/fuzz-hadoop/$testModule/target/meringue/${testClass}/${testMethod}" "result/$testClass/"
 docker stop ${containerName}
 docker rm ${containerName}
