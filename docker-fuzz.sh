b8b376fd952aae538667eeab8bd403c67ea2e125
Tony Wang <tony.wanghao@stu.pku.edu.cn>
2023-03-15 16:46:30 +0800

    update docker fuzz for customizable regex and constraint locations

--- docker-fuzz.sh
+++ docker-fuzz.sh
@@ -1,18 +1,20 @@
 # first run 'docker build -t 'hadoop-build' -f Dockerfile .' from the docker dir
-# Usage: bash docker.sh <testModule> <testClass> <testMethod> <duration>
+# Usage: bash docker.sh <testModule> <testClass> <testMethod> <rootDir> <duration>
 # assumes that constraintFile is named constraint and regex file name
+# root dir is where the two files are stored
 containerName=fuzzingcon
 testModule=$1
 testClass=$2
 testMethod=$3
-constraintFile=constraint
-regexFile=regex
-duration=$4
+rootDir=$4
+constraintFile=$4/constraint
+regexFile=$4/regex
+duration=$5
 
 docker run --name ${containerName} -u ctestfuzz -w "/home/ctestfuzz/fuzz-hadoop/${testModule}" -d -i -t "hadoop-build" bash
-docker exec -u ctestfuzz ${containerName} mvn confuzz:fuzz -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile} -Dmeringue.duration=${duration}
-docker exec -u ctestfuzz ${containerName} mvn confuzz:analyze -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile} -Dmeringue.duration=${duration}
-docker exec -u ctestfuzz ${containerName} mvn confuzz:debug -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile}
+docker exec -u ctestfuzz ${containerName} mvn confuzz:fuzz -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile="${constraintFile}" -DregexFile="${regexFile}" -Dmeringue.duration=${duration}
+docker exec -u ctestfuzz ${containerName} mvn confuzz:analyze -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile="${constraintFile}" -DregexFile="${regexFile}" -Dmeringue.duration=${duration}
+docker exec -u ctestfuzz ${containerName} mvn confuzz:debug -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile="${constraintFile}" -DregexFile="${regexFile}"
 mkdir -p result/$testClass
 docker cp -r "${containerName}:/home/ctestfuzz/fuzz-hadoop/$testModule/target/meringue/${testClass}/${testMethod}" "result/$testClass/"
 docker stop ${containerName}
