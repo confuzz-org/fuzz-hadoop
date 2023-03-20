98397406b3ee85808f42946299ae7b36e240eba8
shuaiwang516 <shuaiwang516@gmail.com>
2023-03-20 01:20:03 -0500

    fix & update docker

--- docker-fuzz.sh
+++ docker-fuzz.sh
@@ -4,19 +4,20 @@
 # root dir is where the two files are stored
 
 # Note that fuzz-hadoop is stored in /home/ctestfuzz/ in the docker
-containerName=fuzzingcon
+containerName=confuzz-container
 testModule=$1
 testClass=$2
 testMethod=$3
 constraintFile=constraint
 regexFile=regex
 duration=$4
+injectConfigFile=$5
 
 docker run --name ${containerName} -u ctestfuzz -w "/home/ctestfuzz/fuzz-hadoop/${testModule}" -d -i -t "hadoop-build" bash
-docker exec -u ctestfuzz ${containerName} mvn confuzz:fuzz -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile="${constraintFile}" -DregexFile="${regexFile}" -Dmeringue.duration=${duration}
-docker exec -u ctestfuzz ${containerName} mvn confuzz:analyze -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile="${constraintFile}" -DregexFile="${regexFile}" -Dmeringue.duration=${duration}
-docker exec -u ctestfuzz ${containerName} mvn confuzz:debug -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile="${constraintFile}" -DregexFile="${regexFile}"
+docker exec -u ctestfuzz ${containerName} mvn confuzz:fuzz -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile} -Dmeringue.duration=${duration}
+docker exec -u ctestfuzz ${containerName} mvn confuzz:analyze -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile} -Dmeringue.duration=${duration}
+docker exec -u ctestfuzz ${containerName} mvn confuzz:debug -Dmeringue.testClass=${testClass} -Dmeringue.testMethod=${testMethod} -DconstraintFile=${constraintFile} -DregexFile=${regexFile} -Dconfig.file=${injectConfigFile}
 mkdir -p result/$testClass
-docker cp -r "${containerName}:/home/ctestfuzz/fuzz-hadoop/$testModule/target/meringue/${testClass}/${testMethod}" "result/$testClass/"
+docker cp ${containerName}:/home/ctestfuzz/fuzz-hadoop/$testModule/target/meringue/${testClass}/${testMethod} result/$testClass/
 docker stop ${containerName}
 docker rm ${containerName}
